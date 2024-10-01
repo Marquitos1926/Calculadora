44e1e4d7986e750a054e8a4b6a702a12f37bf728
Júlio Gabriel <juliogabrieldiasc@gmail.com>
2024-10-01 16:33:42 -0300

    Site Calculadora
    fiz uma calculadora usando java Script, no começo dava erro mas consegui resolver esse erro eu acho...

---
+++ script.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let currentDisplay = document.getElementById('display').value;
+    document.getElementById('display').value = currentDisplay.slice(0, -1);
+}
